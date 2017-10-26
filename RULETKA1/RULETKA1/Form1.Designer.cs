bd46e5585f8796e70fecb3c9be461dccf3031a96
bachkiko <bachkiko95@gmail.com>
2017-10-26 19:22:21 +0300

    ebi se

--- RULETKA1/RULETKA1/Form1.Designer.cs
+++ RULETKA1/RULETKA1/Form1.Designer.cs
@@ -28,128 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> db329b03053aad5775d933bbe5a297e1fb75fd40
             this.button1 = new System.Windows.Forms.Button();
-<<<<<<< HEAD
-            this.label1 = new System.Windows.Forms.Label();
-=======
-            this.button2 = new System.Windows.Forms.Button();
->>>>>>> 200c31af270eff6a01eacc858f2509f8782e0759
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(468, 501);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(351, 407);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(81, 75);
+            this.button1.Size = new System.Drawing.Size(61, 61);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(404, 290);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(669, 527);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-=======
-            this.SuspendLayout();
-            // 
->>>>>>> parent of ffdb357... proba
-<<<<<<< HEAD
-=======
-=======
-            this.SuspendLayout();
-            // 
->>>>>>> parent of ffdb357... proba
->>>>>>> db329b03053aad5775d933bbe5a297e1fb75fd40
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::RULETKA1.Properties.Resources.RULETKATA;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> db329b03053aad5775d933bbe5a297e1fb75fd40
             this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2);
-=======
-            this.ClientSize = new System.Drawing.Size(1685, 838);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.DoubleBuffered = true;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
->>>>>>> 200c31af270eff6a01eacc858f2509f8782e0759
-=======
-            this.ClientSize = new System.Drawing.Size(1262, 673);
-            this.DoubleBuffered = true;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
->>>>>>> parent of ffdb357... proba
-<<<<<<< HEAD
-=======
-=======
-            this.ClientSize = new System.Drawing.Size(1262, 673);
-            this.DoubleBuffered = true;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
->>>>>>> parent of ffdb357... proba
->>>>>>> db329b03053aad5775d933bbe5a297e1fb75fd40
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> db329b03053aad5775d933bbe5a297e1fb75fd40
 
         private System.Windows.Forms.Button button1;
-<<<<<<< HEAD
-        private System.Windows.Forms.Label label1;
-=======
-        private System.Windows.Forms.Button button2;
->>>>>>> 200c31af270eff6a01eacc858f2509f8782e0759
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of ffdb357... proba
-=======
->>>>>>> db329b03053aad5775d933bbe5a297e1fb75fd40
->>>>>>> parent of ffdb357... proba
     }
 }
 
