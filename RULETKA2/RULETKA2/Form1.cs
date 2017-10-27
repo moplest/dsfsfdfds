cb18f70c9325699143b256ea97f38adda3eb1844
bachkiko <bachkiko95@gmail.com>
2017-10-27 14:41:36 +0300

    nova proba

--- RULETKA2/RULETKA2/Form1.cs
+++ RULETKA2/RULETKA2/Form1.cs
@@ -8,23 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace RULETKA1
+namespace RULETKA2
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
- 
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
