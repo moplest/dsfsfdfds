bbd6e5d3622b0488fcca97970eb3acb6a6da250d
kalevinho <bezpatrone@gmail.com>
2017-12-12 12:57:24 +0200

    ASNIE

--- RULETKA/RULETKA/PlayersContainer.cs
+++ RULETKA/RULETKA/PlayersContainer.cs
@@ -12,7 +12,7 @@ namespace RULETKA
         public PlayersContainer()
         {
             Player p1 = new Player("proben user", "1234", 10000);
-            Player p2 = new Player("kalevinho", "951221", 10000);
+            Player p2 = new Player("kalevinho", "951221", 10000000000000000);
             Player p3 = new Player("bacho", "123123", 10000);
             playersContainer = new Player[] { p1, p2, p3 };
         }
